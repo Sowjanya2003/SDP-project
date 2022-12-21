9fb62d39672293853945e0d7a4b8592616247e7e
k.manasa <kalyanammanasa2004@gmail.com>
2022-12-21 14:52:31 +0530

    third commit

--- s9project/app1/urls.py
+++ s9project/app1/urls.py
@@ -3,8 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_view
 urlpatterns = [
         path('', views.mainhome,name='mainhome'),
-        path('register/',views.register,name='register'),
-        path('profile/',views.profile,name='profile'),
+        path('login/',views.login,name='login'),
         path('error/',views.error,name='error'),
         path('home/',views.home,name='home'),
         path('contact/',views.contact,name='contact'),
@@ -12,6 +11,12 @@ urlpatterns = [
         path('table/',views.table,name='table'),
         path('about/',views.about,name='about'),
         path('mainhome/', views.mainhome,name='mainhome'),
-        path('login/', auth_view.LoginView.as_view(template_name='login.html'), name='login'),
-        path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='logout'),
+        path("getres/",views.getres,name="getres"),
+        path('checkuser/',views.checkuser,name="checkuser"),
+        path("gettableres/",views.gettableres,name="gettableres"),
+        path('addtocart/',views.addtocart,name='addtocart'),
+path('addtocart2/',views.addtocart2,name='addtocart2'),
+
+        path('payment/',views.payment,name='payment'),
+
   ]
