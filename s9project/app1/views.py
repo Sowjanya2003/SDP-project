79c93e92f7d4a7c2ffdd4fd58baaefec796ba5bb
k.manasa <kalyanammanasa2004@gmail.com>
2022-12-21 14:52:11 +0530

    third commit

--- s9project/app1/views.py
+++ s9project/app1/views.py
@@ -1,38 +1,27 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import AddTable
-from django.contrib.auth.forms import UserCreationForm
-from . forms import UserRegistrationform
-from . forms import UserLoginform
 
+from pymongo import MongoClient
+
+from .models import AddTable
+
+client=MongoClient("mongodb://127.0.0.1:27017/")
+db=client["SDP"]
+coll=db.stu
+coll2=db.tab
 # Create your views here.
 def mainhome(request):
     return render(request, "mainhome.html")
+
 
 
 def error(request):
     return render(request, "error.html")
 
 
-def register(request):
-    if request.method == "POST":
-        form = UserRegistrationform(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = UserRegistrationform()
-    return render(request, "login.html",{'form':form})
-
 def login(request):
-    if request.method == "POST":
-        form = UserLoginform(request.POST)
-        if form.is_valid():
-            return redirect('home')
-    else:
-        form = UserLoginform
-    return render(request, "login.html",{'form':form})
+    return render(request, "login.html")
 
 
 def home(request):
@@ -46,23 +35,73 @@ def contact(request):
 def menu(request):
     return render(request, "menu.html")
 
-
 def table(request):
-    if request.method == 'POST':
-        name = request.POST['name']
-        email = request.POST['email']
-        date = request.POST['date']
-        time = request.POST['time']
-        branch = request.POST['branch']
-        people = request.POST['people']
-        new_table = AddTable(name=name, email=email, date=date, time=time, branch=branch, people=people)
-        new_table.save()
-    return render(request, 'table.html', {})
-
+    return render(request, 'table.html')
 
 def about(request):
     return render(request, "about.html")
+def addtocart(request):
+    return render(request, "addtocart.html")
+
+def getres(request):
+    if request.method=='POST':
+        user_name=request.POST.get('user_name')
+        email_id=request.POST.get('email_id')
+        pswd=request.POST.get('pswd')
+        l, u, p, d = 0, 0, 0, 0
+        s = pswd
+        if (len(s) >= 6):
+            for i in s:
+
+                # counting lowercase alphabets
+                if (i.islower()):
+                    l += 1
+
+                    # counting uppercase alphabets
+                if (i.isupper()):
+                    u += 1
+
+                    # counting digits
+                if (i.isdigit()):
+                    d += 1
+
+                    # counting the mentioned special characters
+                if (i == '@' or i == '$' or i == '_'):
+                    p += 1
+        if (l >= 1 and u >= 1 and p >= 1 and d >= 1 and l + p + u + d == len(s)):
+            db.coll.insert_one({'user_name': user_name, 'email_id': email_id, 'pswd': pswd})
+            return render(request, "login.html")
+        else:
+            messages.error(request, "Invalid Password!!!  Register Again!!!")
+            return render(request, "login.html")
 
 
-def profile(request):
-    return render(request, "profile.html")
+def checkuser(request):
+    if request.method=='POST':
+        l_username=request.POST.get('l_username')
+        l_pswd=request.POST.get('l_pswd')
+
+        c=db.coll.find_one({'user_name':l_username})
+
+        if c['user_name']==l_username and c['pswd']==l_pswd:
+             return render(request,"home.html")
+        else:
+            return render(request,"login.html")
+    return render(request,"login.html")
+
+def gettableres(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        sdate = request.POST.get('sdate')
+        stime = request.POST.get('stime')
+        branch = request.POST.get('branch')
+        people = request.POST.get('people')
+        db.coll2.insert_one(
+            {'name': name, 'email': email, 'sdate': sdate, 'stime': stime, 'branch': branch, 'people': people})
+        return render(request, "home.html")
+    return render(request, "home.html")
+def addtocart2(request):
+    return render(request, "addtocart2.html")
+def payment(request):
+    return render(request, "payment.html")
